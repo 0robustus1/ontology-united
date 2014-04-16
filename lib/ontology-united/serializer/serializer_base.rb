321ed7aa41f89ebe61c5518a6fefc9698121709f
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-04-16 18:26:41 +0200

    provide serializer base with helper methods
    These helper methods are aimed to allow the following things:
    
    - Join Arrays to strings with intermediate method calls
    - Detect ontology/class/sentence
    - 'process' a specific element, in order to refer to
      it from transfer-context (inside of to_s).

--- lib/ontology-united/serializer/serializer_base.rb
+++ lib/ontology-united/serializer/serializer_base.rb
@@ -1,12 +1,45 @@
 module OntologyUnited
   module Serializer
     class SerializerBase
-      attr_reader :current_ontology, :current_sentence, :current_symbol
+      attr_accessor :current, :parent_current
 
-      def initialize(ontology: nil, sentence: nil, symbol: nil)
-        @current_ontology = ontology
-        @current_sentence = sentence
-        @current_symbol = symbol
+      def initialize(current: nil)
+        self.current = current
+        self.parent_current = nil
+      end
+
+      def process(subject, &block)
+        previous_parent = mark!(subject)
+        result = block.call
+        mark!(parent_current, previous_parent)
+        result
+      end
+
+      def mark!(subject, parent=nil)
+        previous_parent = parent_current
+        parent_current = parent || current
+        current = subject
+        previous_parent
+      end
+
+      def join(elements, sep)
+        elements.reduce('') do |str, raw_element|
+          str << sep unless str.empty?
+          element = yield raw_element
+          str << element
+        end
+      end
+
+      def ontology?(subject)
+        subject.is_a?(OntologyUnited::DSL::Ontology)
+      end
+
+      def class?(subject)
+        subject.is_a?(OntologyUnited::DSL::OntologyClass)
+      end
+
+      def class?(subject)
+        subject.is_a?(OntologyUnited::DSL::OntologySentence)
       end
 
     end
