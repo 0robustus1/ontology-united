f8e307840e21fc827814d1bdd3d04c914f669c6e
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-04-25 00:14:16 +0200

    add spec for importing ontologies.

--- spec/ontology-united/serializer/owl/manchester_spec.rb
+++ spec/ontology-united/serializer/owl/manchester_spec.rb
@@ -57,4 +57,32 @@ describe OntologyUnited::Serializer::OWL::Manchester do
     end
   end
 
+  context 'when serializing ontologies with imports' do
+    let(:manchester) { OntologyUnited::Serializer::OWL::Manchester }
+    let(:name) { 'OntologyName' }
+    let(:list) do
+      referenced, the_import = nil
+      o = OntologyUnited::DSL::OntologyDSL.define("#{name}.owl") do
+        referenced = define('OtherOne.owl') do
+          ontohub = prefix('ontohub', self)
+          ontohub.class('some_class')
+        end
+        the_import = imports referenced
+      end
+      [o, referenced, the_import]
+    end
+    let(:ontology) { list[0] }
+    let(:referenced) { list[1] }
+    let(:import) { list[2] }
+
+    context 'when serializing an import' do
+      let!(:serialized) { import.to_s(serializer: manchester.new) }
+
+      it 'should be a valid manchester syntax import definition' do
+        expect(serialized).to eq("Import: <#{referenced.iri}>")
+      end
+    end
+
+  end
+
 end
