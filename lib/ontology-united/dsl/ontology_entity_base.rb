284f9d0ebf07c3dedb2c2fc3e75adef32e20994b
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-04-28 22:54:40 +0200

    overwrite == instead of eql? for equality
    And alias eql? to ==.

--- lib/ontology-united/dsl/ontology_entity_base.rb
+++ lib/ontology-united/dsl/ontology_entity_base.rb
@@ -5,14 +5,17 @@ module OntologyUnited
       attr_reader :the_calls
 
       def self.delegate_equality_to(method: nil, variable: nil)
-        define_method :eql? do |obj|
+        define_method :== do |obj|
           if method
-            self.send(method.to_sym).eql?(obj.send(method.to_sym))
+            self.send(method.to_sym) == obj.send(method.to_sym)
           elsif variable
             own = self.instance_variable_get(:"@#{variable}")
             other = obj.instance_variable_get(:"@#{variable}")
-            own.eql?(other)
+            own == other
           end
+        end
+        define_method :eql? do |obj|
+          self == obj
         end
         define_method :hash do
           if method
