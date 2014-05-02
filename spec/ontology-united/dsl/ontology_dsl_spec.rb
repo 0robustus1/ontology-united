ffd06164bb66b90b0b61a761b8b60aa6a5e74877
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-05-02 19:48:08 +0200

    add tests for custom variable assignment.

--- spec/ontology-united/dsl/ontology_dsl_spec.rb
+++ spec/ontology-united/dsl/ontology_dsl_spec.rb
@@ -12,6 +12,20 @@ describe OntologyUnited::DSL::OntologyDSL do
     it 'should be allowed to use an ontology argument in the block' do
       expect { ontology }.not_to raise_error
     end
+
+    context 'inner definition of ontology' do
+      let(:outer) do
+        described_class.define('Foo') do
+          define('Bar', as: :bar_ontology) do |this|
+            this.class('Something')
+          end
+        end
+      end
+
+      it 'should contain variable-assignment for inner ontology' do
+        expect(outer.bar_ontology.name).to eq('Bar')
+      end
+    end
   end
 
 end
