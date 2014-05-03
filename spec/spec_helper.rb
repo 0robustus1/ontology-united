2e04609255b830af696749aa4450b8591683cfbe
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-05-03 19:33:31 +0200

    adjust spec helper for local coverage and pry.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
-require 'coveralls'
-Coveralls.wear!
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start
+else
+  require 'coveralls'
+  Coveralls.wear!
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ontology-united'
+require 'pry'
+
