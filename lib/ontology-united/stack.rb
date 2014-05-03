6bc0db1dbad2c7d6699c662671782a41dc98b827
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-05-03 19:35:05 +0200

    add stack delegation mechanic and test it.

--- lib/ontology-united/stack.rb
+++ lib/ontology-united/stack.rb
@@ -13,5 +13,18 @@ module OntologyUnited
       stack[-2]
     end
 
+    module Delegate
+      def delegate_stack_to(method_name=nil, &block)
+        action = ->(stack_operation) do
+          ->() do
+            block.call(self).send(stack_operation)
+          end
+        end
+        define_method :stack, &action[:stack]
+        define_method :current, &action[:current]
+        define_method :parent, &action[:parent]
+      end
+    end
+
   end
 end
--- spec/ontology-united/stack_spec.rb
+++ spec/ontology-united/stack_spec.rb
@@ -53,4 +53,66 @@ describe OntologyUnited::Stack do
     end
   end
 
+  describe OntologyUnited::Stack::Delegate do
+
+    context '.delegate_stack_to' do
+      let(:klass) do
+        class Klass
+          extend OntologyUnited::Stack
+        end
+        Klass
+      end
+
+      let(:subklass) do
+        class SubKlass < klass
+          extend OntologyUnited::Stack::Delegate
+
+          delegate_stack_to { |subklass| subklass.class.superclass }
+        end
+        SubKlass
+      end
+
+      let(:instance) { subklass.new }
+
+      before do
+        klass.instance_variable_set(:@stack, [])
+      end
+
+      context 'alias SubKlass#stack to Klass.stack' do
+        it 'should equal empty stack on default' do
+          expect(instance.stack).to be_empty
+        end
+
+        it 'should equal the superclass stack on push' do
+          klass.stack.push(1)
+          expect(instance.stack).to eq(klass.stack)
+        end
+      end
+
+      context 'alias SubKlass#current to Klass.current' do
+        it 'should equal to empty stack on default' do
+          expect(instance.current).to be_nil
+        end
+
+        it 'should equal the superclass stack on push' do
+          klass.stack.push(1)
+          expect(instance.current).to eq(klass.current)
+        end
+      end
+
+      context 'alias SubKlass#parent to Klass.parent' do
+        it 'should equal nil on default' do
+          expect(instance.parent).to be_nil
+        end
+
+        it 'should equal the superclass stack on push' do
+          klass.stack.push(1, 2)
+          expect(instance.parent).to eq(klass.parent)
+        end
+      end
+
+    end
+
+  end
+
 end
