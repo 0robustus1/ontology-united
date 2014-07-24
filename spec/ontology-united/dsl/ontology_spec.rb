84d429b36aefe54f417e710b3b8a3504bd141a7b
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-07-24 15:59:08 +0200

    add failing test for variable with import
    Currently inner variable assignments
    fails to work correctly if issued as
    part of the import-special syntax.

--- spec/ontology-united/dsl/ontology_spec.rb
+++ spec/ontology-united/dsl/ontology_spec.rb
@@ -21,6 +21,12 @@ describe OntologyUnited::DSL::Ontology do
         this = prefix('this', as: :this_prefix)
         this.class('Something', as: :something_class)
         this.class('Something').sub_class_of this.class('Other'), as: :sub_class_sentence
+
+        imports define('Bar', as: :referenced_ontology) do
+          bar = prefix('this', as: :bar_prefix)
+          bar.class('Something', as: :something_class)
+        end
+
       end
     end
 
@@ -48,6 +54,31 @@ describe OntologyUnited::DSL::Ontology do
       it 'should not contain a nily variable assignment' do
         expect(ontology.sub_class_sentence).to_not be_nil
       end
+    end
+
+    context '.imports' do
+
+      it 'should not contain a nily imported ontology' do
+        expect(ontology.referenced_ontology).to_not be_nil
+      end
+
+      context 'the imported ontology' do
+
+        let(:imported) { ontology.referenced_ontology }
+
+        it 'should have a prefix' do
+          expect(imported.bar_prefix).to_not be_nil
+        end
+
+        it 'should have a a symbol' do
+          expect(imported.something_class).to_not be_nil
+        end
+
+        it 'should have not the same symbol as the parent ontology' do
+          expect(imported.something_class).to_not eq(ontology.something_class)
+        end
+      end
+
     end
 
   end
